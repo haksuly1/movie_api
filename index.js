b1b2b2548774aa4d04fab3d2f11144a810d7982b
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-02-03 18:48:36 +0100

    activated mongodb port

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const Directors = Models.Directors;
 
 //mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-//mongoose.connect("mongodb://localhost:27017/myFlixDB", { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/myFlixDB", { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 app.use(bodyParser.json());
