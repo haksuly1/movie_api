8cf765532229d34400367b2f6f5352827aec0a4b
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-02-04 13:07:58 +0100

    adjusted require Cors

--- index.js
+++ index.js
@@ -35,24 +35,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require("cors");
 app.use(cors()); //This code requires CORS
 
-
-/*
-// Use Cors
-const cors = require("cors");
-let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
-
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-            let message = "The CORS policy for this application doesn’t allow access from origin " + origin;
-            return callback(new Error(message), false);
-        }
-        return callback(null, true);
-    }
-}));
-*/
-
 //log requests to server
 app.use(morgan("common"));
 //Access-Control-Allow-Origin; 
