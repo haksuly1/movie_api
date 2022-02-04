65f3275614cba99d456a41d2db0e87b5d1deb4a6
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-02-04 17:25:38 +0100

    Activated authentication in index.js to test app in postman

--- index.js
+++ index.js
@@ -65,18 +65,18 @@ app.get("/movies", passport.authenticate("jwt", { session: false }), (req, res) 
 
 
 //Get all movies with authentication - Mongoose Models
-//app.get("/movies", 
-//passport.authenticate("jwt", { session: false }), 
-//(req, res) => {
-  //Movies.find()
-    //.then((movies) => {
-      //res.status(201).json(movies); 
-    //})
-    //.catch((err) => { 
-      //console.error(err);
-      //res.status(500).send("Error: " + err);
-    //});
-//});
+app.get("/movies", 
+passport.authenticate("jwt", { session: false }), 
+(req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies); 
+    })
+    .catch((err) => { 
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
 
 
