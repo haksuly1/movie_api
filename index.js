73dbd8132aeb3f9302d8d1fff460c8a63aa8c77c
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-02-08 03:13:42 +0100

    enabled user authentication in index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.get("/movies", passport.authenticate("jwt", { session: false }), (req, res) 
     });
 });
 
-/*
+
 // Get all users - Mongoose Models
 app.get("/users", passport.authenticate("jwt", { session: false }), (req, res) => {
   Users.find()
@@ -81,7 +81,7 @@ app.get("/users", passport.authenticate("jwt", { session: false }), (req, res) =
       res.status(500).send("Error: " + err);
     })
 });
-*/
+
 
 // Get a user by username - Mongoose Models
 app.get("/users/:Username", passport.authenticate("jwt", { session: false }), (req, res) => {
