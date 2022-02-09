1770a27f02df815e7a5c725c84fbaea05fe53c1a
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-02-09 15:26:39 +0100

    Changed allowOrigin style for Cors in index.js

--- index.js
+++ index.js
@@ -43,9 +43,23 @@ let auth = require("./auth")(app); //Require and imports auth.js
 const passport = require("passport"); //Require and import passport.js
 require("./passport");
 
+/*
 //Use Cors
 const cors = require("cors");
 app.use(cors()); //This code requires CORS
+*/
+
+let allowedOrigins = ["http://localhost:8080", "https://haksuly1movieapp.herokuapp.com/", "http://localhost:1234"];
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 //Access-Control-Allow-Origin; 
 // Use Express to return all static files in public folder
