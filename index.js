fe69c20f06798cbdaa7a5655204a9f863a7ddfef
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-02-09 15:29:25 +0100

    udated allowOrigin Cors in index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+      let message = "The CORS policy for this application does not allow access from origin " + origin;
       return callback(new Error(message ), false);
     }
     return callback(null, true);
