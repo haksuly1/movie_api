ab2a5fb9467d1476aa64ea644c1b5a5af267e425
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-02-09 19:56:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -47,20 +47,6 @@ const passport = require("passport"); //Require and import passport.js
 require("./passport");
 let auth = require("./auth")(app); //Require and imports auth.js 
 
-/*
-let allowedOrigins = ["http://localhost:8080", "https://haksuly1movieapp.herokuapp.com/login", "http://localhost:1234"];
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = "The CORS policy for this application does not allow access from origin " + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
-*/
-
 //Access-Control-Allow-Origin; 
 // Use Express to return all static files in public folder
 app.use(express.static("public"));
